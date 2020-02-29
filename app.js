2643b492f6cd7fdf088ebfd3d26f38212e749444
TL Chang <coooo77@hotmail.com>
2020-02-29 16:38:03 +0800

    Add sort feature on index page

--- app.js
+++ app.js
@@ -50,6 +50,54 @@ app.get('/', (req, res) => {
     })
 })
 
+// 取得餐廳A到Z排列
+app.get('/AtoZ', (req, res) => {
+  Restaurant.find()
+    .sort('name')
+    .lean()
+    .exec((err, restaurants) => {
+      if (err) return console.error(err)
+      // console.log(restaurants)
+      return res.render('index', { restaurants })
+    })
+})
+
+// 取得餐廳Z到A排列
+app.get('/ZtoA', (req, res) => {
+  Restaurant.find()
+    .sort('-name')
+    .lean()
+    .exec((err, restaurants) => {
+      if (err) return console.error(err)
+      // console.log(restaurants)
+      return res.render('index', { restaurants })
+    })
+})
+
+// 取得餐廳類別排列
+app.get('/category', (req, res) => {
+  Restaurant.find()
+    .sort('category')
+    .lean()
+    .exec((err, restaurants) => {
+      if (err) return console.error(err)
+      // console.log(restaurants)
+      return res.render('index', { restaurants })
+    })
+})
+
+// 取得餐廳地區排列
+app.get('/location', (req, res) => {
+  Restaurant.find()
+    .sort('location')
+    .lean()
+    .exec((err, restaurants) => {
+      if (err) return console.error(err)
+      // console.log(restaurants)
+      return res.render('index', { restaurants })
+    })
+})
+
 // restaurants搜尋頁面 兩個做法 1.先輸出整份清單，用JS比對 2.用mongodb指令去找
 // 因時間不夠所以先試著用1.，之後回頭研究U82
 app.get('/search', (req, res) => {
